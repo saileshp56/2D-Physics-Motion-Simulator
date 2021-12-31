285708bfb17e72123ab52b9777676eef8d38ec99
saileshp56 <86889128+saileshp56@users.noreply.github.com>
2021-12-31 14:53:57 -0500

    Update TwoDimensionalMotion.java

--- TwoDimensionalMotion.java
+++ TwoDimensionalMotion.java
@@ -1,4 +1,4 @@
-package twodmotion; //I do not have a website, this is just what I named the package
+package twodmotion; //At the time of writing, I do not have a website, this is just what I named the package
 
 import java.io.File;
 import java.io.IOException;
